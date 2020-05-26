40a70aba6893ba0fa91e3a9da97ef0c9818c0f09
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-05-26 18:23:14 -0300

    Updates

---
+++ precache-manifest.f202e59ecc0bdf15fd246f236aac21c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dac4b104c49ab113d70c7a2b79da753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f4dbea46672ae238c75",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27d5323eb11428f48445",
+    "url": "/static/js/2.5b7b86b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b7b86b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4dbea46672ae238c75",
+    "url": "/static/js/main.9aa7fbfc.chunk.js"
+  },
+  {
+    "revision": "a0f33cf4e620b8647bb3",
+    "url": "/static/js/runtime-main.7a0384c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
