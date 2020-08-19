41d395413cb7ad76409afd875fd94a9bba57484e
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-19 17:38:30 -0300

    feat: create Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { AiOutlineGithub, AiOutlineLinkedin } from 'react-icons/ai';
 
-import { Title } from './styles';
+import { Container, Profile, Image, Social, Link } from './styles';
 
 function Home() {
-	return <Title>Home</Title>;
+	return (
+		<Container>
+			<Profile>
+				<Image src="https://www.github.com/atilara.png" alt="Me" />
+				<div>
+					<h1>Olá!</h1>
+					<p>Me chamo Átila Rodrigues! Este é o meu portfólio.</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Enim voluptas dolor voluptatum illum! Dolor totam et
+						hic. Iusto explicabo tempora facilis accusamus
+						laudantium, dignissimos accusantium fugit eligendi
+						velit, magnam est.
+					</p>
+				</div>
+			</Profile>
+			<Social>
+				<Link href="https://www.github.com/atilara" target="_blank">
+					<AiOutlineGithub size={40} />
+					<h3>GitHub</h3>
+				</Link>
+				<Link
+					href="https://www.linkedin.com/in/atilara"
+					target="_blank"
+				>
+					<AiOutlineLinkedin size={40} />
+					<h3>LinkedIn</h3>
+				</Link>
+			</Social>
+		</Container>
+	);
 }
 
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,55 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1``;
+export const Container = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+
+export const Profile = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	max-width: 60rem;
+	p {
+		text-align: justify;
+		margin: 1rem 0;
+	}
+	@media (max-width: 800px) {
+		flex-wrap: wrap;
+		h1 {
+			margin: 0 2rem;
+		}
+		p {
+			margin: 1rem 2rem;
+		}
+	}
+`;
+
+export const Image = styled.img`
+	margin: 2rem;
+	border-radius: 50%;
+	border: 8px solid ${(props) => props.theme.colors.primary};
+	width: 20rem;
+`;
+
+export const Social = styled.div`
+	display: flex;
+	flex-direction: row;
+
+	@media (max-width: 800px) {
+		flex-wrap: wrap;
+	}
+`;
+
+export const Link = styled.a`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin: 0 10px;
+
+	h3 {
+		margin: 0 5px;
+	}
+`;
