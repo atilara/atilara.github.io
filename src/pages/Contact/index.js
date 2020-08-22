16eff9b3b600c6217660e7932b1deed2528bbfd9
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-22 16:46:34 -0300

    feat(pages): create Contact

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import {
+	AiOutlineMail,
+	AiOutlineGithub,
+	AiOutlineLinkedin,
+} from 'react-icons/ai';
 
-import { Title } from './styles';
+import {
+	Container,
+	Info,
+	Social,
+	Link,
+	Form,
+	Input,
+	Textarea,
+	ButtonC,
+} from './styles';
 
 function Contact() {
-	return <Title>Contact</Title>;
+	return (
+		<Container>
+			<Info>
+				<img
+					src="https://www.github.com/atilara.png"
+					alt="Foto de perfil"
+				/>
+				<Social>
+					<div>
+						<AiOutlineMail size={40} />
+						<h3>atila.rodriguesal@gmail.com</h3>
+					</div>
+					<Link href="https://www.github.com/atilara" target="_blank">
+						<AiOutlineGithub size={40} />
+						<h3>atilara</h3>
+					</Link>
+					<Link
+						href="https://www.linkedin.com/in/atilara"
+						target="_blank"
+					>
+						<AiOutlineLinkedin size={40} />
+						<h3>Átila Rodrigues</h3>
+					</Link>
+				</Social>
+			</Info>
+			<Form>
+				<h2>Entre em contato comigo!</h2>
+				<Input type="text" name="name" placeholder="Nome" />
+				<Input type="email" name="email" placeholder="E-mail" />
+				<Input type="text" name="subject" placeholder="Assunto" />
+				<Textarea type="text" name="name" placeholder="Descrição" />
+				<ButtonC>Enviar</ButtonC>
+			</Form>
+		</Container>
+	);
 }
 
 export default Contact;
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,3 +1,116 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1``;
+import Button from '../../components/Button';
+
+export const Container = styled.div`
+	margin-top: 2rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+
+export const Info = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	margin: 0 10rem 1rem 10rem;
+	img {
+		border-radius: 50%;
+		max-width: 16rem;
+		margin-right: 2rem;
+	}
+	@media (max-width: 800px) {
+		flex-wrap: wrap;
+		justify-content: center;
+		img {
+			margin: 0 0 1rem 0;
+			max-width: 12rem;
+		}
+	}
+`;
+
+export const Social = styled.div`
+	display: flex;
+	flex-direction: column;
+	> div {
+		display: flex;
+		flex-direction: row;
+		margin-bottom: 0.6rem;
+		align-items: center;
+		svg {
+			margin-right: 0.4rem;
+		}
+	}
+`;
+
+export const Link = styled.a`
+	display: flex;
+	flex-direction: row;
+	margin-bottom: 0.6rem;
+	align-items: center;
+	svg {
+		margin-right: 0.4rem;
+	}
+`;
+
+export const Form = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	h2 {
+		margin-bottom: 1rem;
+	}
+`;
+
+export const Input = styled.input`
+	width: 60%;
+	margin-bottom: 1rem;
+	padding: 8px 10px;
+	border: 2px solid ${(props) => props.theme.colors.secondary};
+	background-color: ${(props) => props.theme.colors.background};
+	color: ${(props) => props.theme.colors.text};
+	border-radius: 8px;
+	transition: border 0.3s ease;
+
+	&::placeholder {
+		color: ${(props) => props.theme.colors.text};
+	}
+
+	&:focus {
+		outline: 0;
+		border: 2px solid ${(props) => props.theme.colors.primary};
+	}
+	@media (max-width: 800px) {
+		width: 85%;
+	}
+`;
+
+export const Textarea = styled.textarea`
+	width: 60%;
+	height: 8rem;
+	margin-bottom: 1rem;
+	resize: none;
+	padding: 8px 10px;
+	border: 2px solid ${(props) => props.theme.colors.secondary};
+	background-color: ${(props) => props.theme.colors.background};
+	color: ${(props) => props.theme.colors.text};
+	border-radius: 8px;
+	transition: border 0.3s ease;
+	&::placeholder {
+		color: ${(props) => props.theme.colors.text};
+	}
+
+	&:focus {
+		outline: 0;
+		border: 2px solid ${(props) => props.theme.colors.primary};
+	}
+	@media (max-width: 800px) {
+		width: 85%;
+	}
+`;
+
+export const ButtonC = styled(Button)`
+	margin-bottom: 2rem;
+`;
