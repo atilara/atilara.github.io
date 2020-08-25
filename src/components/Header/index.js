9c6422c6e07baedf75f01926a74c8cc7e4d38571
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-08-25 20:35:15 -0300

    fix(components): delete old code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,13 @@
-import React, { useContext } from 'react';
-import Switch from 'react-switch';
-import { ThemeContext } from 'styled-components';
+import React from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi';
 
 import { Container, Nav, Items } from './styles';
 
 function Header({ toggleTheme, theme }) {
-	// const { colors, title } = useContext(ThemeContext);
 	return (
 		<Container>
 			<Nav to="/">Átila Rodrigues</Nav>
 			<Items>
-				{/* <Switch
-					onChange={toggleTheme}
-					checked={title === 'dark'}
-					checkedIcon={false}
-					uncheckedIcon={false}
-					height={10}
-					width={40}
-					handleDiameter={20}
-					onColor={colors.secondary}
-				/> */}
 				<Nav to="/projects">Projetos</Nav>
 				<Nav to="/contact">Contato</Nav>
 				{theme.title === 'light' ? (
