a498f8811e2abef4adbd956af23870e6a284d81d
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-09-27 19:31:26 -0300

    feat(components): add responsiviness to Project

--- src/components/Project/styles.js
+++ src/components/Project/styles.js
@@ -6,12 +6,20 @@ export const Container = styled.div`
 	margin: 0 0 1rem 6rem;
 	display: flex;
 	flex-direction: row;
+	@media (max-width: 700px) {
+		flex-direction: column;
+		margin: 1rem;
+	}
 `;
 
 export const Icon = styled.div`
 	img {
 		width: 10rem;
 		border-radius: 8px;
+		@media (max-width: 700px) {
+			width: 12rem;
+			margin-bottom: 0.8rem;
+		}
 	}
 `;
 
@@ -23,13 +31,22 @@ export const Content = styled.div`
 
 export const Title = styled.h3`
 	margin: 0 0 0.4rem 1rem;
+	@media (max-width: 700px) {
+		margin: 0 0 0.4rem 0;
+	}
 `;
 
 export const Description = styled.p`
 	margin: 0 0 0.4rem 1rem;
+	@media (max-width: 700px) {
+		margin: 0 0 0.4rem 0;
+	}
 `;
 
 export const ButtonS = styled(Button)`
 	margin-left: 1rem;
 	padding: 8px;
+	@media (max-width: 700px) {
+		margin: 0 0 2rem 0;
+	}
 `;
