523bd164597bb2af083d0b9ee2da01361b48c4f7
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-09-27 19:05:54 -0300

    refactor(components): modify padding in Header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -8,13 +8,15 @@ export const Container = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-	padding: 0 20px;
+	padding: 0 6rem;
 	position: relative;
 	> svg {
 		display: none;
 	}
 
 	@media (max-width: 700px) {
+		padding: 0 20px;
+
 		> svg {
 			display: block;
 		}
