c7c9405dc446f70830089d59f99cdbc953e43b70
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-09-27 19:22:25 -0300

    feat(pages): add Project component to the Projects page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 import { Title } from './styles';
 
+import Project from '../../components/Project';
+
 function Projects() {
-	return <Title>Projects</Title>;
+	return (
+		<>
+			<Title>Projetos</Title>
+			<Project />
+			<Project />
+			<Project />
+		</>
+	);
 }
 
 export default Projects;
--- src/pages/Projects/styles.js
+++ src/pages/Projects/styles.js
@@ -1,3 +1,5 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+	margin: 1rem 0 1rem 6rem;
+`;
